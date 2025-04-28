caeaa8e5fc96ba637a9ce6fec7b8cd55292d10aa
Marta Marín Lorente <martamarinlorente@gmail.com>
2025-04-28 12:11:03 +0200

    Subida

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btnCiudad = document.getElementById("btn-ciudad");
+
+btnCiudad.addEventListener("click", function(){
+    // Vuelve a la página de inicio
+    window.location.href = "../../index.html";
+});
+
