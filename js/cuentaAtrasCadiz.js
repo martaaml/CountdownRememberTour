b1f6842078767606d7fccbe4f131553aaeb0c074
Marta Marín Lorente <martamarinlorente@gmail.com>
2025-04-30 12:28:37 +0200

    Prueba confeti

--- js/cuentaAtrasCadiz.js
+++ js/cuentaAtrasCadiz.js
@@ -1,5 +1,5 @@
 // Fecha objetivo
-const fechaObjetivo = new Date("Apr 30, 2025 14:30:00").getTime();
+const fechaObjetivo = new Date("Apr 30, 2025 12:30:00").getTime();
 
 // Lógica de la cuenta atrás
 const intervalo = setInterval(() => {
